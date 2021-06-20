3247482785d1cca13f46bace8ad28480f02eeccd
Javier Oswaldo <javiersacor201430061@cunoc.edu.gt>
2021-06-20 16:48:11 -0600

    tablero ordenar fichas

--- src/Tablero.java
+++ src/Tablero.java
@@ -44,4 +44,27 @@ public class Tablero
             System.out.println();
         }
     }    
+        
+    void ordenarFichas()
+    {
+        for (int i = 0; i < 3; i = i + 1) 
+        {
+            for (int j = 0; j < celdasPorLado; j = j + 1)
+            {
+                if(!celdas[i][j].getEsCeldaColorA())
+                {
+                    celdas[i][j].setFichaColor1(false);
+                }
+            }
+
+            for (int j = 0; j < celdasPorLado; j = j + 1)
+            {
+                if(!celdas[celdasPorLado - 1 - i][j].getEsCeldaColorA())
+                {
+                    celdas[celdasPorLado - 1 - i][j].setFichaColor1(true);
+                }
+            }
+        }
+        imprimirTablero();
+    }
 }
