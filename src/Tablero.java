a3f341e1f2abf0a96fc9ad8d0d23ef6243ec8a6d
Javier Oswaldo <javiersacor201430061@cunoc.edu.gt>
2021-06-20 16:46:10 -0600

    tablero constructor

--- src/Tablero.java
+++ src/Tablero.java
@@ -4,4 +4,22 @@ public class Tablero
 {
     private Celda[][] celdas;
     private int celdasPorLado;
+
+    public Tablero(int celdasPorLado) 
+    {
+        this.celdasPorLado = celdasPorLado;
+        celdas = new Celda[celdasPorLado][celdasPorLado];
+
+        for (int i = 0; i < celdasPorLado; i = i + 1) 
+        {
+            boolean iniciaColorA = i % 2 == 0 ? true : false;
+
+            for (int j = 0; j < celdasPorLado; j = j + 1)
+            {
+                boolean esColorA = iniciaColorA ? j % 2 == 0 : j % 2 == 1;
+
+                celdas[i][j] = new Celda(esColorA);
+            }
+        }
+    }
 }
